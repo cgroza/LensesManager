159b4689e075cc3cff33d0da9146ca3f2e6bf6dd
Groza Cristian <kristi9524@gmail.com>
2012-02-04 17:51:40 -0500

    completed lens reading functions.

--- LenseTools.hs
+++ LenseTools.hs
@@ -6,25 +6,40 @@ import qualified System.IO.Strict as IOS
 import System.IO
 import Text.Regex
 import Data.List
+import Data.Maybe
+import qualified Data.Map as M
 
+type Lens = M.Map Field Value
 
-data Lens = Lens {name :: String , path :: FilePath } deriving Show
-
-data Field = Icon | Name | Description | SearchHint | Shortcut | Visible deriving Show
+data Field = Icon | Name | Description | SearchHint | Shortcut | Visible | Path  deriving (Show, Eq, Ord)
 type Value = String
 
 lensDir = "/usr/share/unity/lenses"
 
 changeLens :: Lens -> Field -> Value -> IO ()
 changeLens lens field val = do
-  writeFile (path lens) . flip (subRegex regex)  (show field ++ "=" ++val) =<< IOS.readFile (path lens)
+  writeFile path . flip (subRegex regex)  (show field ++ "=" ++val) =<< IOS.readFile path
+  where regex = mkRegex $ "^" ++ show field ++ ".*=.*$"
+        path = fromJust $ M.lookup Path lens
+
+getAttr :: String -> Field -> Value
+getAttr txt field = 
+  case matchRegexAll regex txt of
+    Just (_, s, _, _) -> if not $ null s then  
+                splitRegex (mkRegex "=") ( s) !! 1
+              else ""
+    Nothing ->  ""
   where regex = mkRegex $ "^" ++ show field ++ ".*=.*$"
 
 getLens :: FilePath -> IO [Lens]
-getLens filePath = (return .  map readLens . map ((lensDir </>) . (filePath </>)) . filter isLens) =<< (getDirectoryContents (lensDir </> filePath))
-  where isLens = isSuffixOf ".lens" 
-        readLens f = let name = dropExtension $ snd $ splitFileName f
-                         path = f in Lens name path
+getLens filePath = (mapM readLens . map ((lensDir </>) . (filePath </>)) . filter isLens) =<< (getDirectoryContents (lensDir </> filePath))
+  where fields = [Icon , Name , Description , SearchHint , Shortcut , Visible]
+        isLens = isSuffixOf ".lens" 
+        readLens :: FilePath -> IO Lens
+        readLens file = do
+          contents <- readFile file
+          return $ M.insert Path file $ M.fromList $ zip fields $ map (getAttr contents) fields 
+          
 
 getAllLens :: IO [Lens]
 getAllLens = return . concat =<< mapM getLens =<< getDirectoryContents lensDir
--- LensesManager.hs
+++ LensesManager.hs
@@ -1,35 +1,61 @@
 module Main where
 import Graphics.UI.Gtk
 import Graphics.UI.Gtk.Glade
+import Graphics.UI.Gtk.Gdk.Pixbuf
 import LenseTools
+import qualified Data.Map as M
+import Data.Maybe
 
-data GUI = GUI {lensesView :: IconView}
 
-populateIconView :: IO ()
-populateIconView = undefined
+data GUI = GUI {lensesView :: IconView, lensesStore :: ListStore (String, Pixbuf)}
 
-showEditWindow :: IO ()
-showEditWindow = undefined
+populateIconView :: GUI -> IO ()
+populateIconView gui = do     
+  mapM_ (addIcon (lensesStore gui)) =<< getAllLens
+  where addIcon lstore lens = do  
+          p <- pixbufNewFromFile $ fromJust $ M.lookup LenseTools.Icon lens
+          listStoreAppend lstore (fromJust $ M.lookup Name lens , p)
+
+
+showEditWindow :: GUI -> IO ()
+showEditWindow GUI {lensesView = lview, lensesStore = lstore} = do
+  selected <- treeRowReferenceNew lstore . head =<< iconViewGetSelectedItems lview 
+  case selected of
+    Just treeRef -> do val <- listStoreGetValue lstore . head =<< treeRowReferenceGetPath treeRef
+                       print $ fst val
+    Nothing -> return ()
+  
 
 main = 
   do
     initGUI
     Just xml <- xmlNew "GUI.glade"
     window   <- xmlGetWidget xml castToWindow "top_window"
+
     widgetSetSizeRequest window 300 350
     onDestroy window mainQuit
 
+  -- setup the icon view
+    lensesView <- xmlGetWidget xml castToIconView "lenses_view"
+    storeSource <- listStoreNew ([] :: [(String, Pixbuf)]) 
+    treeModelSetColumn storeSource (makeColumnIdString 1) fst
+    treeModelSetColumn storeSource (makeColumnIdPixbuf 2) snd
+    iconViewSetModel lensesView (Just storeSource)
+    iconViewSetTextColumn lensesView (makeColumnIdString 1)
+    iconViewSetPixbufColumn lensesView (makeColumnIdPixbuf 2)
+    iconViewSetModel lensesView (Just storeSource)
+
+    let gui = GUI lensesView storeSource
+
     editButton <- xmlGetWidget xml castToButton "edit_button"
-    onClicked editButton showEditWindow
+    onClicked editButton $ showEditWindow gui
 
     okButton <- xmlGetWidget xml castToButton "ok_button"
     onClicked okButton mainQuit
 
-    lensesView <- xmlGetWidget xml castToIconView "lenses_view"
-    let gui = GUI lensesView
+    populateIconView gui
 
     widgetShowAll window
-
     lens <- getAllLens
     mapM_ print lens
 
